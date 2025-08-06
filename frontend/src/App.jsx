b3f1b6e89ad4838c05ece21fec12354dbbc4f226
Fariha alam mozumder <156440883+Fariha-alam-mozumder@users.noreply.github.com>
2025-08-07 03:29:00 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import LandingPage from "./AuthenticationPages/LandingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 export default function App() {
   return (
     <Router>
