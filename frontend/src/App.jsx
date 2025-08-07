fbdc3747237c1b582c214cf209688cb476800885
erina akter <erinaakter2001@gmail.com>
2025-08-08 03:05:53 +0600

    added reviewer full code and changed app.jsx accordingly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
-import LandingPage from "./AuthenticationPages/LandingPage";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReviewerDashboard from './Pages/Reviewer/reviewerDashboard';
+import ReviewerHome from './Pages/Reviewer/ReviewerHome';
+import AssignedPapersPage from './Pages/Reviewer/AssignedPapersPage';
+import AssignedProposalsPage from './Pages/Reviewer/AssignedProposalsPage';
+import PaperReviewPage from './Pages/Reviewer/PaperReviewPage';
+import AdminDashboard from './Pages/Admin/AdminDashboard';
+import ReviewHistoryPage from './Pages/Reviewer/ReviewHistoryPage';
 
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+       
+        <Route path="/ReviewerDashboard" element={<ReviewerDashboard />}>
+          <Route index element={<ReviewerHome />} />
+          <Route path="assignedpapers" element={<AssignedPapersPage />} />
+          <Route path="assigned-proposals" element={<AssignedProposalsPage />} />
+          <Route path="review/:paperId" element={<PaperReviewPage />} />
+          <Route path="ReviewHistoryPage" element={<ReviewHistoryPage />} />
+        </Route>
+         <Route path="/AdminDashboard" element={<AdminDashboard />} />
 
       </Routes>
     </Router>
