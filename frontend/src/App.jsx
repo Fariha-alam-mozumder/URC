c74a9d67855c039f3540d0bbba71f473fa478b0f
FARIHA alam mozumder <farihaalam57@gmail.com>
2025-08-08 18:19:19 +0600

    added landingpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/home/Homepage";
+import LandingPage from "./pages/landingpage/LandingPage";
 
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      
+       <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Homepage />} />
       </Routes>
     </Router>
