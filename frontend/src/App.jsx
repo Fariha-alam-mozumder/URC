2476cf2004d39eb2dfb13c4b7baea5df2f666172
FARIHA alam mozumder <farihaalam57@gmail.com>
2025-08-08 16:48:06 +0600

    Your commit message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import LandingPage from "./AuthenticationPages/LandingPage";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/home/Homepage";
 
 
-export default function App() {
+
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-
+      
+        <Route path="/home" element={<Homepage />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
 
 
---
+++ frontend/src/pages/landingpage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import HeroSection from '../../components/landingPage/HeroSection';
+
+function LandingPage() {
+  return (
+    <div>
+      <HeroSection/>
+    </div>
+  );
+}
+export default LandingPage;
