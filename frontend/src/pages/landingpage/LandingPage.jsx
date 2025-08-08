a6260ea33d00ec4fdd21672f84fa6377b6363a26
FARIHA alam mozumder <farihaalam57@gmail.com>
2025-08-08 18:19:19 +0600

    added landingpage

--- frontend/src/pages/landingpage/LandingPage.jsx
+++ frontend/src/pages/landingpage/LandingPage.jsx
@@ -1,9 +1,19 @@
+import AcceptedPaper from '../../components/home/AcceptedPaper';
+import Conferences from '../../components/home/Conferences';
+import Footer from '../../components/landingPage/FooterSection';
 import HeroSection from '../../components/landingPage/HeroSection';
+import JoinSection from '../../components/landingPage/JoinSection';
+import LockSection from '../../components/landingPage/LockSection';
 
 function LandingPage() {
   return (
     <div>
       <HeroSection/>
+      <AcceptedPaper/>
+      <Conferences/>
+      <LockSection/>
+      <JoinSection/>
+      <Footer/>
     </div>
   );
 }
