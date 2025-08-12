6906c81e0a87d618527e471c9aaacee8e632e491
elmahtoasean <toasean.csecu@gmail.com>
2025-08-13 00:38:29 +0600

    Edited login payload

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -198,6 +198,7 @@ class AuthController {
         name: findUser.name,
         email: findUser.email,
         role: findUser.role,
+        emailVerified: findUser.isVerified,
       };
 
       const token = jwt.sign(payloadData, process.env.JWT_SECRET, {
