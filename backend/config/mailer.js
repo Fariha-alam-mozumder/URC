397d33ae4d7703f13b04bdabd1a4c6221adc1c70
elmahtoasean <157800316+elmahtoasean@users.noreply.github.com>
2025-09-23 10:31:36 +0600

    Update mailer.js

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,10 +1,10 @@
 import nodemailer from "nodemailer"
 
-// Create a test account or replace with real credentials.
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
@@ -16,6 +16,6 @@ export const sendEmail = async (toMail, subject, body) => {
     from: process.env.FROM_EMAIL,
     to: toMail,
     subject: subject,
-    html: body, // HTML body
+    html: body, 
     });
 }
